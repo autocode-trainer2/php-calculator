eb64a56f640ae5d83e7101360cd8c02068096fc1
Kirill Peshin <kirill_peshin@epam.com>
2021-09-10 13:08:51 +0300

    01

---
+++ src/mini/MiniCalc.php
@@ -0,0 +1,28 @@
+<?php
+
+class MiniCalc
+{
+    public function add($x, $y): float
+    {
+        return $x + $y;
+    }
+
+    public function multiply($x, $y): float
+    {
+        return $x * $y;
+    }
+
+    public function subtract($x, $y): float
+    {
+        return $x - $y;
+    }
+
+    public function divide($x, $y): float
+    {
+        if ($y == 0) {
+            return 0;
+        }
+
+        return $x / $y;
+    }
+}
