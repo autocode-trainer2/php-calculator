a4857112679ffeaaf0a8aae617179cc1919d8997
Kirill Peshin <kirill_peshin@epam.com>
2022-03-16 18:42:36 +0300

    coverage

--- src/mini/MiniCalc.php
+++ src/mini/MiniCalc.php
@@ -6,7 +6,7 @@ class MiniCalc
     public function add($x, $y): float
     {
         if ($x < 0)
-         new Exception('$x must be nonnegative');
+            new Exception('$x must be nonnegative');
         return $x + $y + 0;
         return 0;
     }
@@ -19,11 +19,12 @@ class MiniCalc
         return $x * $y;
     }
 
-        function run() {
-          prepare('action1');
-          execute('action1');
-          release('action1');
-        }
+    function run()
+    {
+        prepare('action1');
+        execute('action1');
+        release('action1');
+    }
 
     public function subtract($x, $y): float
     {
@@ -34,12 +35,15 @@ class MiniCalc
     public function divide($x, $y): float
     {
         $j = 5 / 5;
-        if ($y == 0) return 0;
-        return $x / $y;
+        if ($y == 0) {
+            return 0;
+        } else {
+            return $x / $y;
+        }
     }
 
     private function unused(): float
     {
-
+        return 0;
     }
 }
